e5893dff62d0595230eb9e87926ba04f7babd418
em-lily <imlilycohen@gmail.com>
2022-01-29 20:56:34 +0000

    overcompleted crm activity

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,72 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
-
+DROP TABLE IF EXISTS salespeople;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS associations;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS myself;
+DROP TABLE IF EXISTS memberships;
 -- CREATE TABLES
+
+CREATE TABLE salespeople (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    email TEXT,
+    phone_number TEXT,
+    company_id INTEGER
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    salesperson_id INTEGER,
+    contact_id INTEGER,
+    created_at TEXT,
+    notes TEXT
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE associations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    contact_id INTEGER,
+    company_id INTEGER
+);
+
+CREATE TABLE industries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE myself (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE memberships (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
+    industry_id INTEGER
+);
+
+SELECT * FROM contacts
+INNER JOIN companies ON ...
+INNER JOIN memberships ON ...
+INNER JOIN industries ON ...
+WHERE industries.name='Technology'
